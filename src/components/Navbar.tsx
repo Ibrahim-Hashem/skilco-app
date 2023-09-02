cebc2c21243979d1656517354652a076a2018916
Ibrahim Hashem <ibrahimsgh6@hotmail.co.uk>
2023-09-02 23:48:44 +0100

    implemented vote and comment feature

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,12 @@
-import Link from "next/link";
-import { Icons } from "./Icons";
-import { buttonVariants } from "./ui/Button";
-import { getAuthSession } from "@/lib/auth";
-import UserAccountNav from "@/components/UserAccountNav";
+import Link from 'next/link'
+import { Icons } from './Icons'
+import { buttonVariants } from './ui/Button'
+import { getAuthSession } from '@/lib/auth'
+import UserAccountNav from '@/components/UserAccountNav'
+import SearchBar from '@/components/SearchBar'
 
 const Navbar = async () => {
-  const session = await getAuthSession();
+  const session = await getAuthSession()
   return (
     <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
       <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
@@ -14,6 +15,7 @@ const Navbar = async () => {
           <Icons.logo className="w-14 h-8 sm:w-16 sm:h-8 " />
         </Link>
         {/* search bar */}
+        <SearchBar />
         {/* auth */}
         {session?.user ? (
           <UserAccountNav user={session?.user} />
@@ -24,6 +26,6 @@ const Navbar = async () => {
         )}
       </div>
     </div>
-  );
-};
-export default Navbar;
+  )
+}
+export default Navbar
--- src/components/PostComment.tsx
+++ src/components/PostComment.tsx
@@ -1,8 +1,21 @@
 'use client'
-import { FC, useRef } from 'react'
+import { FC, startTransition, useRef, useState } from 'react'
 import UserAvatar from './UserAvatar'
 import { CommentVote, User, Comment } from '@prisma/client'
 import { formatTimeToNow } from '@/lib/utils'
+import CommentVotes from './CommentVotes'
+import { Button } from './ui/Button'
+import { MessageSquare } from 'lucide-react'
+import { Icons } from './Icons'
+import { useRouter } from 'next/navigation'
+import { useSession } from 'next-auth/react'
+import { boolean } from 'zod'
+import { Textarea } from './ui/Textarea'
+import { Label } from './ui/Label'
+import { CommentRequest } from '@/lib/validators/comment'
+import axios from 'axios'
+import { useMutation } from '@tanstack/react-query'
+import { toast } from '@/hooks/use-toast'
 
 type ExtendedComment = Comment & {
   votes: CommentVote[]
@@ -10,10 +23,51 @@ type ExtendedComment = Comment & {
 }
 interface PostCommentProps {
   comment: ExtendedComment
+  votesAmt: number
+  currentVote: CommentVote | undefined
+  postId: string
 }
 
-const PostComment: FC<PostCommentProps> = ({ comment }) => {
+const PostComment: FC<PostCommentProps> = ({
+  comment,
+  votesAmt,
+  currentVote,
+  postId,
+}) => {
   const postCommentRef = useRef<HTMLDivElement>(null)
+  const router = useRouter()
+  const { data: session } = useSession()
+  const [isReplying, setIsReplying] = useState<boolean>(false)
+  const [input, setInput] = useState<string>('')
+
+  const { mutate: postComment, isLoading } = useMutation({
+    mutationFn: async ({ postId, text, replyToId }: CommentRequest) => {
+      const payload: CommentRequest = {
+        postId,
+        text,
+        replyToId,
+      }
+      const { data } = await axios.patch(`/api/project/post/comment`, payload)
+      return data
+    },
+    onError: (err) => {
+      return toast({
+        title: 'Ohhh no!',
+        description: 'Something went wrong while posting your comment.',
+        variant: 'destructive',
+      })
+    },
+    onSuccess: (data) => {
+      startTransition(() => {
+        router.refresh()
+        setIsReplying(false)
+      })
+      toast({
+        title: 'Comment posted',
+        variant: 'default',
+      })
+    },
+  })
   return (
     <div className="flex flex-col" ref={postCommentRef}>
       <div className="flex items-center">
@@ -34,6 +88,54 @@ const PostComment: FC<PostCommentProps> = ({ comment }) => {
         </div>
       </div>
       <p className="text-sm text-zinc-900 mt-2">{comment.text}</p>
+      <div className="flex gap-2 items-center flex-wrap">
+        <CommentVotes
+          commentId={comment.id}
+          initialVotesAmt={votesAmt}
+          initialVote={currentVote}
+        />
+        <Button
+          variant="ghost"
+          size="sm"
+          onClick={() => {
+            if (!session) router.push('/sign-in')
+            setIsReplying((prev) => !prev)
+            setInput('')
+          }}
+        >
+          <Icons.messageSquare className="h-4 w-4 text-zinc-700 mr-4" />
+          Reply
+        </Button>
+        {isReplying ? (
+          <div className="grid w-full gap-1.5">
+            <Label htmlFor="comment">Your Comment</Label>
+
+            <Textarea
+              id="comment"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              rows={1}
+              placeholder="Comment..."
+            />
+            <div className="mt-2 flex justify-end">
+              <Button
+                isLoading={isLoading}
+                disabled={input.length === 0}
+                onClick={() => {
+                  if (!input) return
+                  postComment({
+                    postId,
+                    text: input,
+                    replyToId: comment.replytoId || undefined,
+                  })
+                }}
+              >
+                {!isLoading && <Icons.send />}
+              </Button>
+            </div>
+          </div>
+        ) : null}
+      </div>
     </div>
   )
 }
